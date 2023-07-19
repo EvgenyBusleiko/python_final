ca2dcf40c5cc59da94795396723021e3448553d8
EvgenyBusleiko <busleiko@mail.ru>
2023-07-19 17:23:23 +0300

    add ht10

---
+++ sem10_ht1.py
@@ -0,0 +1,83 @@
+# Доработать задачи 5-6. Создайте класс-фабрику.
+# ○ Класс принимает тип животного (название одного из созданных классов)
+# и параметры для этого типа.
+# ○ Внутри класса создайте экземпляр на основе переданного типа и
+# верните его из класса-фабрики.
+import sys
+
+
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+
+class Horse(Animal):
+    def __init__(self, name, age, power):
+        super().__init__(name, age)
+        self.power = power
+
+    def get_name(self):
+        return 'Secret!'
+
+    def get_power(self):
+        return self.power
+
+
+class Fish(Animal):
+    def __init__(self, name, age, tail_length):
+        super().__init__(name, age)
+        self.tail_length = tail_length
+
+
+class Bird(Animal):
+    def __init__(self, name, age, fly_speed):
+        super().__init__(name, age)
+        self.fly_speed = fly_speed
+
+    def fly_speed(self):
+        return self.fly_speed
+
+
+class Animalmaker:
+
+    def __init__(self, animal_type: Animal, name, age, extra):
+        self.animal_type = animal_type
+        self.name = name
+        self.age = age
+        self.extra = extra
+
+    def make_class(self):
+        type_list = ['Bird', 'Horse', 'Fish']
+
+        tmp = self.animal_type(self.name, self.age, self.extra)
+
+        # if self.animal_type not in type_list:
+        #     print('Такого класса нет')
+        #     return
+        # else:
+        #     class_name = (f'{self.animal_type}("{self.name}",{self.age},{self.extra})')
+        #     tmp = eval(class_name)
+        return tmp
+
+
+a1 = Animalmaker(Bird, 'Po-po', 12, 46).make_class()
+print(a1.get_name())
+print(a1.get_age())
+print(a1.fly_speed)
+a2 = Animalmaker(Horse, 'Flower', 10, 'bite').make_class()
+print(a2.get_name())
+print(a2.get_age())
+print(a2.power)
+a3 = Animalmaker(Fish, 'Ponio', 1, 6).make_class()
+print(a3.get_name())
+print(a3.get_age())
+print(a3.tail_length)
+
+# print(a1.get_name())
---
+++ sem10_ht2.py
@@ -0,0 +1,37 @@
+# Прочитайте созданный в прошлом задании csv файл без использования csv.DictReader.
+# Дополните id до 10 цифр незначащими нулями.
+# В именах первую букву сделайте прописной.
+# Добавьте поле хеш на основе имени и идентификатора.
+# Получившиеся записи сохраните в json файл, где каждая строка csv файла представлена как отдельный json словарь.
+# Имя исходного и конечного файлов передавайте как аргументы функции.
+# Возьмите 1-3 любые задачи из прошлых семинаров (например сериализация
+# данных), которые вы уже решали. Превратите функции в методы класса, а
+# параметры в свойства. Задачи должны решаться через вызов методов
+# экземпляра.
+
+import json
+import csv
+
+
+class Csv_file:
+    def __init__(self, name):
+        self.name = name
+
+    def convert(self):
+        data = {}
+        with open(self.name, encoding='UTF-8') as f1:
+            for line in f1.readlines()[1:]:
+                level, access_id, name = line.strip().split()
+                # print(level, access_id, name)
+                # access_id = f'{int(access_id):010}'
+                # print(access_id)
+                name = name.capitalize()
+                # h_name = hash(name + access_id)
+                # print(h_name)
+                data[access_id] = [level, name]
+            # print(data)
+        with open('res3.json', 'w', encoding='UTF-8') as f2:
+            json.dump(data, f2, ensure_ascii=False)
+
+
+Csv_file('res3.csv').convert
